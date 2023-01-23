3061e0085a889795383f1476c1b1b6e32420da47
Igor <klevzhits94@icloud.com>
2023-01-23 22:31:44 +0300

    Complite task 4

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] getNewArray(string[] arrayStrings)
+{
+    int lengthNewArray = 0;
+
+    for (int i = 0; i < arrayStrings.Length; i++)
+    {
+        if (arrayStrings[i].Length <= 3) 
+        {
+            lengthNewArray++;
+        }
+    }
+
+    string[] result = new string[lengthNewArray];
+    int j = 0;
+
+    for (int i = 0; i < arrayStrings.Length; i++)
+    {
+        if (arrayStrings[i].Length <= 3) 
+        {
+            result[j] = arrayStrings[i];
+            j++;
+        }
+    }
+    return result;
+}
+
+void showArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]} ]");
+}
+
+string[] arrayStrings = { "Hello", "Hi", "Tom", "Cool", "Bottom", "Key"};
+string[] resultArray = getNewArray(arrayStrings);
+showArray(arrayStrings);
+Console.Write(" -> ");
+showArray(resultArray);
+Console.WriteLine();
